3114c4e0f942bd6d1cf7d6278af5420465b8c983
sakura067m <48373188+sakura067m@users.noreply.github.com>
2019-12-02 17:57:16 +0900

    hotfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 ls $GITHUB_WORKSPACE
 ls $GITHUB_WORKSPACE/paper
-pandoc $1 -f markdown -o $2 --metadata-file=$3
+sh -c 'pandoc $1 -f markdown -o $2 --metadata-file=$3'
 ls $GITHUB_WORKSPACE/output
