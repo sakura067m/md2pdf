ccfd890a7b7aa400cb8f2b5613d0ee9c292cee1c
sakura067m <3IE19001M@s.kyushu-u.ac.jp>
2019-12-03 17:55:05 +0900

    hotfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ path_output=$PWD/$3
 echo ::set-output name=path::$path_output
 cd $1;
 
-if [ $# -le 3]; then
+if [ $# -le 3 ]; then
     pandoc $2 -f markdown -o $path_output
 else
     pandoc $2 -f markdown -o $path_output --default=$4
